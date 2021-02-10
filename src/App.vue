9b57aeaa8532788cc5fab2e2b376e30918bfcb5f
nhat.tran <nhat.tran@encapital.io>
2021-02-10 17:08:54 +0700

    feat: add helper validator props string

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div style="text-align: center">
     <h1>Các loại bánh truyền thống ở Việt Nam</h1>
-    <VietnameseCake cake="XXX" />
+    <!-- <VietnameseCake :cake="Cake.XXX" /> -->
+    <VietnameseCake :cake="Cake.BanhChung" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Cake } from './types'
+</script>
